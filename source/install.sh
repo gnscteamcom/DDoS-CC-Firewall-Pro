93bea97f2b72665469b381496ee94eab32aa5046
HKLCF <hklcf@users.noreply.github.com>
2016-12-29 16:21:15 +0800

    Create install.sh

---
+++ source/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# DDOS & CC Firewall Pro installation wrapper
+# https://www.vpshouse.pro
+
+#
+# Currently Supported Operating Systems:
+#
+#   RHEL 5, RHEL 6
+#   CentOS 5, CentOS 6, CentOS 7
+#   Debian 7
+#
+
+# Check OS type
+if [ -e '/etc/redhat-release' ]; then
+    type="rhel"
+fi
+if [ -z "$type" ]; then
+    os=$(head -n1 /etc/issue | cut -f 1 -d ' ')
+    if [ "$os" == 'Debian' ]; then
+        type="debian"
+    fi
+fi
+# Check type
+if [ -z "$type" ]; then
+    echo 'Error: only RHEL,CentOS,Debian 7 is supported'
+    exit 1
+fi
+# Check wget
+if [ -e '/usr/bin/wget' ]; then
+    wget http://mirror.vpshouse.net/firewall/install-rhel.sh
+    chmod 0700 install-rhel.sh
+    if [ "$?" -eq '0' ]; then
+        bash install-rhel.sh $*
+        exit
+    else
+        echo "Error: install-rhel.sh download failed."
+        exit 1
+    fi
+fi
+# Let's try to install wget automaticaly
+if [ "$type" = 'rhel' ]; then
+    ./install-rhel.sh
+    if [ $? -ne 0 ]; then
+        echo "Error: can't install wget"
+        exit 1
+    fi
+fi
+# OK, last try
+if [ -e '/usr/bin/wget' ]; then
+    wget http://mirror.vpshouse.net/firewall/install-rhel.sh
+    if [ "$?" -eq '0' ]; then
+        bash install-rhel.sh $*
+        exit
+    else
+        echo "Error: install-rhel.sh download failed."
+        exit 1
+    fi
+else
+    echo "Error: /usr/bin/wget not found"
+    exit 1
+fi
+exit
